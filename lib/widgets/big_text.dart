c4cddf3f95b9b0c4c3a0d1049d68dc6904a401b9
Samuel Adeniji <57541118+zammie01@users.noreply.github.com>
2022-04-28 21:38:42 +0100

    initial commit

---
+++ lib/widgets/big_text.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:food_order/utils/dimesions.dart';
+
+class BigText extends StatelessWidget {
+  final Color? color;
+  final String text;
+  double size;
+  TextOverflow overFlow;
+
+  BigText({
+    Key? key,
+    this.color = const Color(0xFF332d2b),
+    required this.text,
+    this.size = 0,
+    this.overFlow = TextOverflow.ellipsis,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      maxLines: 1,
+      overflow: overFlow,
+      style: TextStyle(
+        color: color,
+        fontFamily: 'Roboto',
+        fontWeight: FontWeight.w400,
+        fontSize: size == 0 ? Dimensions.font20 : size,
+      ),
+    );
+  }
+}
